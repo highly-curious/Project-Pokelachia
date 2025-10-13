f17f1e9db8841cc5c124bc98525782a47a205dfe
highly-curious <highly.curious@outlook.com>
2025-10-13 16:16:07 -0400

    sram update

--- ram/sram.asm
+++ ram/sram.asm
@@ -24,7 +24,7 @@ sGameDataEnd::
 
 sOptions3:: db
 
-	ds 396 ; unused
+	ds 397 ; unused
 
 sSRAMAccessCount:: db
 
@@ -53,7 +53,7 @@ sBackupGameDataEnd::
 
 sBackupOptions3:: db
 
-	ds 397 ; unused
+	ds 398 ; unused
 
 sBackupChecksum:: dw
 	assert sBackupChecksum == $bf0d, "Backup checksum has shifted."
